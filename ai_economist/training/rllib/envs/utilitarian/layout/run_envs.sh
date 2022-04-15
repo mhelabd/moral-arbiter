a0068552d003b7780a9b981aafbcaec4d911d7cf
Michael Elabd <mhelabd@stanford.edu>
2022-04-14 19:15:43 -0700

    Standardized bash scripts and tqdm

--- ai_economist/training/rllib/envs/utilitarian/layout/run_envs.sh
+++ ai_economist/training/rllib/envs/utilitarian/layout/run_envs.sh
@@ -1,10 +1,10 @@
 cd ~/ai-ethicist/ai_economist/training/rllib/
-for config_file in $(ls  -d envs/utilitarian/layout/agent*)
+for config_file in $(ls  -d envs/utilitarian/layout/agent* |  tqdm --total $(ls  -d envs/utilitarian/layout/agent* | wc -l));
 do
     python training_script.py --run-dir ${config_file} > ${config_file}/logfile.txt 2>&1&
     while ! tail -1 ${config_file}/logfile.txt | grep "Final snapshot saved! All done." > /dev/null;
     do
         echo "working..."
-        sleep 120;
+        sleep 5;
     done
 done
--- ai_economist/training/rllib/envs/utilitarian/layout/test_envs.sh
+++ ai_economist/training/rllib/envs/utilitarian/layout/test_envs.sh
@@ -1,5 +1,5 @@
 cd ~/ai-ethicist/ai_economist/training/rllib/
-for config_file in $(ls  -d envs/utilitarian/layout/agent*)
+for config_file in $(ls  -d envs/utilitarian/layout/agent* |  tqdm --total $(ls  -d envs/utilitarian/layout/agent* | wc -l));
 do
     python training_script.py --run-dir ${config_file}/predefined_skill > ${config_file}/predefined_skill/logfile.txt 2>&1&
     while ! tail -1 ${config_file}/predefined_skill/logfile.txt | grep "Final snapshot saved! All done." > /dev/null;
