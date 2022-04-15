9634d9b610b418a563a9b150a71c5b51fb0fbb16
Michael Elabd <mhelabd@stanford.edu>
2022-04-15 09:57:27 -0700

    tqdm bash scripts

--- ai_economist/training/rllib/envs/virtue_ethics/layout/run_envs.sh
+++ ai_economist/training/rllib/envs/virtue_ethics/layout/run_envs.sh
@@ -1,10 +1,9 @@
 cd ~/ai-ethicist/ai_economist/training/rllib/
-for config_file in $(ls  -d envs/virtue_ethics/layout/agent* |  tqdm --total $(ls  -d envs/virtue_ethics/layout/agent* | wc -l));
+for config_file in $(ls  -d envs/virtue_ethics/layout/agent*);
 do
     python training_script.py --run-dir ${config_file} > ${config_file}/logfile.txt 2>&1&
     while ! tail -1 ${config_file}/logfile.txt | grep "Final snapshot saved! All done." > /dev/null;
     do
-        echo "working..."
         sleep 5;
     done
-done
+done |  tqdm --total $(ls  -d envs/virtue_ethics/layout/agent* | wc -l)  >> /dev/null
